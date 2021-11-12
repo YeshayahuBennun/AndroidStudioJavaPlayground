2ebe021d4bf034d7a252adcc7afdf8706b3be9ab
YeshayahuBennun <ybennun@yahoo.com>
2021-11-12 15:34:00 +0200

    commit#02 - JavaPlayground - Hello World

---
+++ LearnJava/src/main/java/com/ybennun/learnjava/MyClass.java
@@ -0,0 +1,9 @@
+package com.ybennun.learnjava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        String name = "Carlos";
+
+        System.out.println("Hello my name is " + name);
+    }
+}
