9fd26a7994a7e47126cad50d078dcfe08df14839
YeshayahuBennun <ybennun@yahoo.com>
2021-11-26 14:42:32 +0200

    commit#014 - JavaPlayground -  Function Parameters - Add Method

--- LearnJava/src/main/java/com/ybennun/learnjava/JavaMethodsAndParameters.java
+++ LearnJava/src/main/java/com/ybennun/learnjava/JavaMethodsAndParameters.java
@@ -1,13 +1,27 @@
 package com.ybennun.learnjava;
 
+import com.sun.tools.javac.comp.Todo;
+
 public class JavaMethodsAndParameters {
     public static void main(String[] args) {
-        anotherMethod("Hello");
+        addNumbers(12,12);
+        anotherMethod("Hello", 23);
         showName();
     }
 
-    public static void anotherMethod(String something) {
-        System.out.println("Another Method " + something);
+    // TODO: 26/11/2021 create a multiplyNumbers(),divideNumbers()...
+    public static void addNumbers(int a, int b) {
+        int sum = a+b;
+        System.out.println("Sum is: " + sum);
+    }
+
+    public static void anotherMethod(String something, int age) {
+
+        if (age < 90) {
+            System.out.println("I'm still young");
+        }
+
+        System.out.println("Another Method " + something + age);
     }
 
     public static void showName() {
