9d9df9fcf4fa591fe7170aaa0f46194b38c7422a
YeshayahuBennun <ybennun@yahoo.com>
2021-11-27 18:28:36 +0200

    commit#015 - JavaPlayground -  Return Types in Methods

--- LearnJava/src/main/java/com/ybennun/learnjava/JavaMethodsAndParameters.java
+++ LearnJava/src/main/java/com/ybennun/learnjava/JavaMethodsAndParameters.java
@@ -4,15 +4,26 @@ import com.sun.tools.javac.comp.Todo;
 
 public class JavaMethodsAndParameters {
     public static void main(String[] args) {
-        addNumbers(12,12);
+        myName("Paulo");
+        System.out.println(addNumbers(12, 12));
         anotherMethod("Hello", 23);
         showName();
+        System.out.println(showFullName("George", "The man"));
+        System.out.println(showChar('B'));
     }
 
     // TODO: 26/11/2021 create a multiplyNumbers(),divideNumbers()...
-    public static void addNumbers(int a, int b) {
-        int sum = a+b;
+    public static void myName(String mName) {
+        System.out.println(mName);
+    }
+
+    //Return a integer
+    public static int addNumbers(int a, int b) {
+        int sum = a + b;
+        int result = sum;
         System.out.println("Sum is: " + sum);
+
+        return sum; //(a+b)
     }
 
     public static void anotherMethod(String something, int age) {
@@ -31,5 +42,15 @@ public class JavaMethodsAndParameters {
             }
         }
         System.out.println("From showName");
+    }
+
+    //Return a string
+    public static String showFullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+
+    //Return a Character
+    public static char showChar(char c) {
+        return c;
     }
 }
